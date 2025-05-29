4301895990c2c2c1234b549ff63a4896a013ff4a
Charlotte Moche <charlottebmoche@gmail.com>
2025-05-29 10:46:49 -0400

    css

--- frontend/src/pages/TicketDashboard/index.tsx
+++ frontend/src/pages/TicketDashboard/index.tsx
@@ -69,7 +69,7 @@ export function TicketDashboard() {
 				</div>
 			</div>
 
-			<div class="flex flex-col md:flex-row gap-10 lg:gap-20">
+			<div class="flex flex-col md:flex-row gap-10 lg:gap-14 xl:gap-20">
 				<section class="md:basis-1/4">
 					<MetricsPanel />
 				</section>
@@ -84,14 +84,14 @@ export function TicketDashboard() {
 							{filter === 'all' ? 'No tickets.' : `No ${filter} tickets.`}
 						</p>
 					) : (
-						<ul class="grid md:grid-cols-2 gap-6">
+						<ul class="grid lg:grid-cols-2 gap-6">
 							{filteredTickets.map((ticket) => (
 								<li key={ticket.id} class="border border-gray-100 shadow dark:border-gray-600 rounded dark:bg-dark-2 bg-white">
 									<TicketInfo ticket={ticket} showFullDetails={false} />
 									<div class="border-t dark:border-gray-600 px-3 pb-3">
 										<div class="flex items-center justify-between">
 											<TicketActions ticket={ticket} onAction={(type, note) => handleAction(ticket.id, type, note)} />
-											<div class="flex items-center gap-2 mt-4">
+											<div class="flex items-center gap-1 mt-4">
 												<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
 													<path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
 												</svg>
